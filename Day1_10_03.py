0c4acb203a43f03a6e0deda5d8b65af859992251
cherry0097 <163658602+cherry0097@users.noreply.github.com>
2025-03-10 12:00:51 +0530

    Update 12:00 PM IST

--- Day1_10_03.py
+++ Day1_10_03.py
@@ -1,3 +1,291 @@
 '''
 This is a comment.
 '''
+
+''' Prblem 1: Print this poem as output as it is:
+জটাটবীগলজ্জলপ্রবাহপাবিতস্থলে
+গলেবলংব্য় লংবিতাং ভুজংগতুংগমালিকাম্ ।
+ডমড্ডমড্ডমড্ডমন্নিনাদবড্ডমর্বয়ং
+চকার চংডতাংডবং তনোতু নঃ শিবঃ শিবম্ ॥ 1 ॥
+
+জটাকটাহসংভ্রমভ্রমন্নিলিংপনির্ঝরী-
+-বিলোলবীচিবল্লরীবিরাজমানমূর্ধনি ।
+ধগদ্ধগদ্ধগজ্জ্বলল্ললাটপট্টপাবকে
+কিশোরচংদ্রশেখরে রতিঃ প্রতিক্ষণং মম ॥ 2 ॥
+
+ধরাধরেংদ্রনংদিনীবিলাসবংধুবংধুর
+স্ফুরদ্দিগংতসংততিপ্রমোদমানমানসে ।
+কৃপাকটাক্ষধোরণীনিরুদ্ধদুর্ধরাপদি
+ক্বচিদ্দিগংবরে মনো বিনোদমেতু বস্তুনি ॥ 3 ॥
+
+জটাভুজংগপিংগলস্ফুরত্ফণামণিপ্রভা
+কদংবকুংকুমদ্রবপ্রলিপ্তদিগ্বধূমুখে ।
+মদাংধসিংধুরস্ফুরত্ত্বগুত্তরীযমেদুরে
+মনো বিনোদমদ্ভুতং বিভর্তু ভূতভর্তরি ॥ 4 ॥
+
+সহস্রলোচনপ্রভৃত্যশেষলেখশেখর
+প্রসূনধূলিধোরণী বিধূসরাংঘ্রিপীঠভূঃ ।
+ভুজংগরাজমালয়া নিবদ্ধজাটজূটক
+শ্রিয়ৈ চিরায় জাযতাং চকোরবংধুশেখরঃ ॥ 5 ॥
+
+ললাটচত্বরজ্বলদ্ধনংজযস্ফুলিংগভা-
+-নিপীতপংচসাযকং নমন্নিলিংপনাযকম্ ।
+সুধাময়ূখলেখয়া বিরাজমানশেখরং
+মহাকপালিসংপদেশিরোজটালমস্তু নঃ ॥ 6 ॥
+
+করালফালপট্টিকাধগদ্ধগদ্ধগজ্জ্বল-
+দ্ধনংজয়াধরীকৃতপ্রচংডপংচসাযকে ।
+ধরাধরেংদ্রনংদিনীকুচাগ্রচিত্রপত্রক-
+-প্রকল্পনৈকশিল্পিনি ত্রিলোচনে মতির্মম ॥ 7 ॥
+
+নবীনমেঘমংডলী নিরুদ্ধদুর্ধরস্ফুরত্-
+কুহূনিশীথিনীতমঃ প্রবংধবংধুকংধরঃ ।
+নিলিংপনির্ঝরীধরস্তনোতু কৃত্তিসিংধুরঃ
+কলানিধানবংধুরঃ শ্রিয়ং জগদ্ধুরংধরঃ ॥ 8 ॥
+
+প্রফুল্লনীলপংকজপ্রপংচকালিমপ্রভা-
+-বিলংবিকংঠকংদলীরুচিপ্রবদ্ধকংধরম্ ।
+স্মরচ্ছিদং পুরচ্ছিদং ভবচ্ছিদং মখচ্ছিদং
+গজচ্ছিদাংধকচ্ছিদং তমংতকচ্ছিদং ভজে ॥ 9 ॥
+
+অগর্বসর্বমংগলাকলাকদংবমংজরী
+রসপ্রবাহমাধুরী বিজৃংভণামধুব্রতম্ ।
+স্মরাংতকং পুরাংতকং ভবাংতকং মখাংতকং
+গজাংতকাংধকাংতকং তমংতকাংতকং ভজে ॥ 10 ॥
+
+জযত্বদভ্রবিভ্রমভ্রমদ্ভুজংগমশ্বস-
+-দ্বিনির্গমত্ক্রমস্ফুরত্করালফালহব্যবাট্ ।
+ধিমিদ্ধিমিদ্ধিমিধ্বনন্মৃদংগতুংগমংগল
+ধ্বনিক্রমপ্রবর্তিত প্রচংডতাংডবঃ শিবঃ ॥ 11 ॥
+
+দৃষদ্বিচিত্রতল্পয়োর্ভুজংগমৌক্তিকস্রজোর্-
+-গরিষ্ঠরত্নলোষ্ঠয়োঃ সুহৃদ্বিপক্ষপক্ষয়োঃ ।
+তৃষ্ণারবিংদচক্ষুষোঃ প্রজামহীমহেংদ্রয়োঃ
+সমং প্রবর্তযন্মনঃ কদা সদাশিবং ভজে ॥ 12 ॥
+
+কদা নিলিংপনির্ঝরীনিকুংজকোটরে বসন্
+বিমুক্তদুর্মতিঃ সদা শিরঃস্থমংজলিং বহন্ ।
+বিমুক্তলোললোচনো ললাটফাললগ্নকঃ
+শিবেতি মংত্রমুচ্চরন্ সদা সুখী ভবাম্যহম্ ॥ 13 ॥
+
+ইমং হি নিত্যমেবমুক্তমুত্তমোত্তমং স্তবং
+পঠন্স্মরন্ব্রুবন্নরো বিশুদ্ধিমেতিসংততম্ ।
+হরে গুরৌ সুভক্তিমাশু যাতি নান্যথা গতিং
+বিমোহনং হি দেহিনাং সুশংকরস্য় চিংতনম্ ॥ 14 ॥
+
+পূজাবসানসময়ে দশবক্ত্রগীতং যঃ
+শংভুপূজনপরং পঠতি প্রদোষে ।
+তস্য় স্থিরাং রথগজেংদ্রতুরংগয়ুক্তাং
+লক্ষ্মীং সদৈব সুমুখিং প্রদদাতি শংভুঃ ॥ 15 ॥
+'''
+
+'''Answer'''
+
+print('''
+জটাটবীগলজ্জলপ্রবাহপাবিতস্থলে
+গলেবলংব্য় লংবিতাং ভুজংগতুংগমালিকাম্ ।
+ডমড্ডমড্ডমড্ডমন্নিনাদবড্ডমর্বয়ং
+চকার চংডতাংডবং তনোতু নঃ শিবঃ শিবম্ ॥ 1 ॥
+
+জটাকটাহসংভ্রমভ্রমন্নিলিংপনির্ঝরী-
+-বিলোলবীচিবল্লরীবিরাজমানমূর্ধনি ।
+ধগদ্ধগদ্ধগজ্জ্বলল্ললাটপট্টপাবকে
+কিশোরচংদ্রশেখরে রতিঃ প্রতিক্ষণং মম ॥ 2 ॥
+
+ধরাধরেংদ্রনংদিনীবিলাসবংধুবংধুর
+স্ফুরদ্দিগংতসংততিপ্রমোদমানমানসে ।
+কৃপাকটাক্ষধোরণীনিরুদ্ধদুর্ধরাপদি
+ক্বচিদ্দিগংবরে মনো বিনোদমেতু বস্তুনি ॥ 3 ॥
+
+জটাভুজংগপিংগলস্ফুরত্ফণামণিপ্রভা
+কদংবকুংকুমদ্রবপ্রলিপ্তদিগ্বধূমুখে ।
+মদাংধসিংধুরস্ফুরত্ত্বগুত্তরীযমেদুরে
+মনো বিনোদমদ্ভুতং বিভর্তু ভূতভর্তরি ॥ 4 ॥
+
+সহস্রলোচনপ্রভৃত্যশেষলেখশেখর
+প্রসূনধূলিধোরণী বিধূসরাংঘ্রিপীঠভূঃ ।
+ভুজংগরাজমালয়া নিবদ্ধজাটজূটক
+শ্রিয়ৈ চিরায় জাযতাং চকোরবংধুশেখরঃ ॥ 5 ॥
+
+ললাটচত্বরজ্বলদ্ধনংজযস্ফুলিংগভা-
+-নিপীতপংচসাযকং নমন্নিলিংপনাযকম্ ।
+সুধাময়ূখলেখয়া বিরাজমানশেখরং
+মহাকপালিসংপদেশিরোজটালমস্তু নঃ ॥ 6 ॥
+
+করালফালপট্টিকাধগদ্ধগদ্ধগজ্জ্বল-
+দ্ধনংজয়াধরীকৃতপ্রচংডপংচসাযকে ।
+ধরাধরেংদ্রনংদিনীকুচাগ্রচিত্রপত্রক-
+-প্রকল্পনৈকশিল্পিনি ত্রিলোচনে মতির্মম ॥ 7 ॥
+
+নবীনমেঘমংডলী নিরুদ্ধদুর্ধরস্ফুরত্-
+কুহূনিশীথিনীতমঃ প্রবংধবংধুকংধরঃ ।
+নিলিংপনির্ঝরীধরস্তনোতু কৃত্তিসিংধুরঃ
+কলানিধানবংধুরঃ শ্রিয়ং জগদ্ধুরংধরঃ ॥ 8 ॥
+
+প্রফুল্লনীলপংকজপ্রপংচকালিমপ্রভা-
+-বিলংবিকংঠকংদলীরুচিপ্রবদ্ধকংধরম্ ।
+স্মরচ্ছিদং পুরচ্ছিদং ভবচ্ছিদং মখচ্ছিদং
+গজচ্ছিদাংধকচ্ছিদং তমংতকচ্ছিদং ভজে ॥ 9 ॥
+
+অগর্বসর্বমংগলাকলাকদংবমংজরী
+রসপ্রবাহমাধুরী বিজৃংভণামধুব্রতম্ ।
+স্মরাংতকং পুরাংতকং ভবাংতকং মখাংতকং
+গজাংতকাংধকাংতকং তমংতকাংতকং ভজে ॥ 10 ॥
+
+জযত্বদভ্রবিভ্রমভ্রমদ্ভুজংগমশ্বস-
+-দ্বিনির্গমত্ক্রমস্ফুরত্করালফালহব্যবাট্ ।
+ধিমিদ্ধিমিদ্ধিমিধ্বনন্মৃদংগতুংগমংগল
+ধ্বনিক্রমপ্রবর্তিত প্রচংডতাংডবঃ শিবঃ ॥ 11 ॥
+
+দৃষদ্বিচিত্রতল্পয়োর্ভুজংগমৌক্তিকস্রজোর্-
+-গরিষ্ঠরত্নলোষ্ঠয়োঃ সুহৃদ্বিপক্ষপক্ষয়োঃ ।
+তৃষ্ণারবিংদচক্ষুষোঃ প্রজামহীমহেংদ্রয়োঃ
+সমং প্রবর্তযন্মনঃ কদা সদাশিবং ভজে ॥ 12 ॥
+
+কদা নিলিংপনির্ঝরীনিকুংজকোটরে বসন্
+বিমুক্তদুর্মতিঃ সদা শিরঃস্থমংজলিং বহন্ ।
+বিমুক্তলোললোচনো ললাটফাললগ্নকঃ
+শিবেতি মংত্রমুচ্চরন্ সদা সুখী ভবাম্যহম্ ॥ 13 ॥
+
+ইমং হি নিত্যমেবমুক্তমুত্তমোত্তমং স্তবং
+পঠন্স্মরন্ব্রুবন্নরো বিশুদ্ধিমেতিসংততম্ ।
+হরে গুরৌ সুভক্তিমাশু যাতি নান্যথা গতিং
+বিমোহনং হি দেহিনাং সুশংকরস্য় চিংতনম্ ॥ 14 ॥
+
+পূজাবসানসময়ে দশবক্ত্রগীতং যঃ
+শংভুপূজনপরং পঠতি প্রদোষে ।
+তস্য় স্থিরাং রথগজেংদ্রতুরংগয়ুক্তাং
+লক্ষ্মীং সদৈব সুমুখিং প্রদদাতি শংভুঃ ॥ 15 ॥
+''')
+
+'''Problem 2: Use an external module and use it to perform an operation of your interest.'''
+
+'''Answer
+To achive the above I am installing PyJokes using: pip install pyjokes
+PS A:\Learning\Python> pip install pyjokes
+Collecting pyjokes
+  Downloading pyjokes-0.8.3-py3-none-any.whl.metadata (3.4 kB)
+Downloading pyjokes-0.8.3-py3-none-any.whl (47 kB)
+Installing collected packages: pyjokes
+Successfully installed pyjokes-0.8.3
+
+[notice] A new release of pip is available: 24.2 -> 25.0.1
+[notice] To update, run: python.exe -m pip install --upgrade pip
+'''
+
+import pyjokes
+print(pyjokes.get_joke())
+
+'''
+There is another interesting module known as pyttsx3: pip install pyttsx3
+Text to Speech (TTS) library for Python 3. Works without internet connection or delay. Supports multiple TTS engines, including Sapi5, nsss, and espeak.
+'''
+
+'''
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say('''
+# Jatatavigalajjala pravahapavitasthale
+# Galeavalambya lambitam bhujangatungamalikam
+# Damad damad damaddama ninadavadamarvayam
+# Chakara chandtandavam tanotu nah shivah shivam
+
+# Jata kata hasambhrama bhramanilimpanirjhari
+# Vilolavichivalarai virajamanamurdhani
+# Dhagadhagadhagajjva lalalata pattapavake
+# Kishora chandrashekhare ratih pratikshanam mama
+
+# Dharadharendrana ndinivilasabandhubandhura
+# Sphuradigantasantati pramodamanamanase
+# Krupakatakshadhorani nirudhadurdharapadi
+# Kvachidigambare manovinodametuvastuni
+
+# Jata bhujan gapingala sphuratphanamaniprabha
+# Kadambakunkuma dravapralipta digvadhumukhe
+# Madandha sindhu rasphuratvagutariyamedure
+# Mano vinodamadbhutam bibhartu bhutabhartari
+
+# Sahasra lochana prabhritya sheshalekhashekhara
+# Prasuna dhulidhorani vidhusaranghripithabhuh
+# Bhujangaraja malaya nibaddhajatajutaka
+# Shriyai chiraya jayatam chakora bandhushekharah
+
+# Lalata chatvarajvaladhanajnjayasphulingabha
+# nipitapajnchasayakam namannilimpanayakam
+# Sudha mayukha lekhaya virajamanashekharam
+# Maha kapali sampade shirojatalamastunah
+
+# Karala bhala pattikadhagaddhagaddhagajjvala
+# Ddhanajnjaya hutikruta prachandapajnchasayake
+# Dharadharendra nandini kuchagrachitrapatraka
+# Prakalpanaikashilpini trilochane ratirmama
+
+# navina megha mandali niruddhadurdharasphurat
+# Kuhu nishithinitamah prabandhabaddhakandharah
+# nilimpanirjhari dharastanotu krutti sindhurah
+# Kalanidhanabandhurah shriyam jagaddhurandharah
+
+# Praphulla nila pankaja prapajnchakalimchatha
+# Vdambi kanthakandali raruchi prabaddhakandharam
+# Smarachchidam purachchhidam bhavachchidam makhachchidam
+# Gajachchidandhakachidam tamamtakachchidam bhaje
+
+# Akharvagarvasarvamangala kalakadambamajnjari
+# Rasapravaha madhuri vijrumbhana madhuvratam
+# Smarantakam purantakam bhavantakam makhantakam
+# Gajantakandhakantakam tamantakantakam bhaje
+
+# Jayatvadabhravibhrama bhramadbhujangamasafur
+# Dhigdhigdhi nirgamatkarala bhaal havyavat
+# Dhimiddhimiddhimidhva nanmrudangatungamangala
+# Dhvanikramapravartita prachanda tandavah shivah
+
+# Drushadvichitratalpayor bhujanga mauktikasrajor
+# Garishtharatnaloshthayoh suhrudvipakshapakshayoh
+# Trushnaravindachakshushoh prajamahimahendrayoh
+# Sama pravartayanmanah kada sadashivam bhaje
+
+# Kada nilimpanirjhari nikujnjakotare vasanh
+# Vimuktadurmatih sada shirah sthamajnjalim vahanh
+# Vimuktalolalochano lalamabhalalagnakah
+# Shiveti mantramuchcharan sada sukhi bhavamyaham
+
+# Imam hi nityameva muktamuttamottamam stavam
+# Pathansmaran bruvannaro vishuddhimeti santatam
+# Hare gurau subhaktimashu yati nanyatha gatim
+# Vimohanam hi dehinam sushankarasya chintanam
+''')
+engine.runAndWait()
+
+'''
+
+'''
+Problem 3: Write a python program to add 2 numbers:
+'''
+
+a = int(input("Type the 1st number: "))
+b = int(input("Type the 2nd number: "))
+
+print(f"The sum of {a} and {b} is {a + b} and avarage is: {(a + b) // 2} and squre of 1st number is {a**2} and 2nd number is {b**2}")
+
+if a > b: print(f"1st number is grater then 2nd")
+if a == b: print(f"1st number is equal to 2nd")
+if a < b: print(f"1st number is less then 2nd")
+
+'''
+Problem 4: Write a python program to find reminder when devided by z
+'''
+
+c = a + b
+
+z = int(input("Type the number of Z: "))
+print(f"The reminder is: {c % z}")
+
+'''
+Problem 4: Write a python program to check the type of variables using input function
+'''
+
+d = input("Type here: ")
+
+print(f"Type of default input function is: {type(d)}")
