73d30094f4ee9529c1379292bafc7af325ef020b
cherry0097 <163658602+cherry0097@users.noreply.github.com>
2025-03-30 12:48:41 +0530

    30-03-2025

--- Operators.py
+++ Operators.py
@@ -35,10 +35,91 @@ Now we will discuss the most important part: Bitwise operators:
 '''
 
 a = 23
-operator = ""
+b = 25
 
-while a > 0:
-    operator += str(a % 2)
-    a //= 2
-    
-print(operator[::-1])
+
+
+def decmialtobinary(n):
+    operator = ""
+    while n > 0:
+        operator += str(n % 2)
+        n //= 2
+        
+    for i in operator[::-1]:
+        print(i,end=" | ")
+
+# Now we will play with this:
+
+print("Bitwise operators:")
+print()
+
+decmialtobinary(a)
+print(a)
+decmialtobinary(b)
+print(b)
+
+c = a & b
+
+print("AND---------------------------")
+decmialtobinary(c)
+print(c)
+
+print()
+decmialtobinary(a)
+print(a)
+decmialtobinary(b)
+print(b)
+
+c = a | b
+
+print("OR----------------------------")
+decmialtobinary(c)
+print(c)
+
+print()
+decmialtobinary(a)
+print(a)
+decmialtobinary(b)
+print(b)
+
+c = a ^ b
+
+print("XOR---------------------------")
+decmialtobinary(c)
+print(c)
+
+print()
+decmialtobinary(a)
+print(a)
+
+c = a >> 2
+
+print("Shift 2 place right------------")
+decmialtobinary(c)
+print(c)
+
+print()
+decmialtobinary(a)
+print(a)
+
+c = a << 2
+
+print("Shift 2 place left------------")
+decmialtobinary(c)
+print(c)
+
+print()
+decmialtobinary(a)
+print(a)
+
+c = ~a
+
+print("Negetive 23 using ~------------")
+decmialtobinary(c)
+print(c)
+
+
+
+
+
+
