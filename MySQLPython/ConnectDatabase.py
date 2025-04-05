4d714b305c7f8b1e06638dcf7272cbe6a3a6eadf
cherry0097 <163658602+cherry0097@users.noreply.github.com>
2025-04-05 20:48:02 +0530

    Updated files

---
+++ MySQLPython/ConnectDatabase.py
@@ -0,0 +1,6 @@
+'''
+We need to install mysql.connector using: pip install mysql-connector-python
+However, for this project I will install the mysql-connector-python on my virtual environment.
+To do this I will create a virtual environment using the command: python -m venv MySQLenv
+'''
+import mysql.connector as mysql
